6d85fe854ca9689e9c5e683c6a3a594a8fe005f1
kufreudoko <kufre.udoko@stanbicibtc.com>
2024-08-12 16:20:38 +0100

    LinkedList

--- src/main/java/com/kelmorgan/datastrutcureandalgorithm/Main.java
+++ src/main/java/com/kelmorgan/datastrutcureandalgorithm/Main.java
@@ -1,10 +1,17 @@
 package com.kelmorgan.datastrutcureandalgorithm;
 
-import com.kelmorgan.datastrutcureandalgorithm.array.Array;
+import com.kelmorgan.datastrutcureandalgorithm.linkedlist.LinkedList;
+
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
 
+        var list = new LinkedList();
+
+        list.addLast(10);
+        list.addFirst(20);
+        list.addLast(30);
     }
 }
