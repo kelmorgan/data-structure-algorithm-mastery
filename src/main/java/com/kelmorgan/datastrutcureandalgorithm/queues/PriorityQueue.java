4a5f2d61f90ded7c19cbc4a3f440738f4b73bb07
kufreudoko <kufre.udoko@stanbicibtc.com>
2024-08-21 12:15:43 +0100

    queues

--- src/main/java/com/kelmorgan/datastrutcureandalgorithm/queues/PriorityQueue.java
+++ src/main/java/com/kelmorgan/datastrutcureandalgorithm/queues/PriorityQueue.java
@@ -1,21 +1,54 @@
 package com.kelmorgan.datastrutcureandalgorithm.queues;
 
+import java.util.Arrays;
+
 public class PriorityQueue {
 
 
-    private int [] items  = new int[5];
+    private int[] items = new int[5];
 
     private int count;
 
 
-    public void add(int item){
-        if (count == items.length)
+    public void add(int item) {
+        if (isFull())
             throw new IllegalStateException();
 
+        int i = shiftItemsToInsert(item);
+        items[i] = item;
+        count++;
+    }
 
+    private boolean isFull() {
+        return count == items.length;
+    }
 
-        for (int i = count - 1; i >=0; i--) {
+    private int shiftItemsToInsert(int item) {
+        int i;
 
+        //shifting
+        for (i = count - 1; i >= 0; i--) {
+            if (items[i] > item)
+                items[i + 1] = items[i];
+            else
+                break;
         }
+        return i + 1;
+    }
+
+    public int remove(){
+        if (isEmpty())
+            throw new IllegalStateException();
+
+        return items[--count];
+    }
+
+    public boolean isEmpty(){
+        return count == 0;
+    }
+
+    @Override
+    public String toString(){
+        return Arrays.toString(items);
     }
 }
